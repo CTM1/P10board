9798c47b1458d8c5459eb97454b29bf063266898
krkn <claudio.t1337@gmail.com>
2021-07-24 16:36:48 +0200

    Adapted server for OSC protocol, added string parsing in Arduino

--- P10_server/P10_receive/P10_receive.ino
+++ P10_server/P10_receive/P10_receive.ino
@@ -3,6 +3,7 @@
 #include <TimerOne.h>
 #include <SystemFont5x7.h>
 #include "Arial_black_16.h"
+#include <string.h>
 
 #define ROW 1
 #define COL 1
@@ -44,15 +45,40 @@ void print_scrolling_text(char* c, int speed)
 void loop()
 {
   if (Serial.available() == 0)
-  {
     print_scrolling_text("https://192.168.43.118:8080", 30);
-  }
+
   
   if (Serial.available() > 0)
   {
-    String inByte = Serial.readString();
-    char* c = inByte.c_str();
-    
-    print_scrolling_text(c, 30);
+    String request = Serial.readString();
+    char* request_id  = request.c_str();
+    char* strToken =  strtok(request_id, ",");
+
+    switch(request_id[0])
+    {
+      case '0': //Scrolling text
+        strToken = strtok(NULL, ",");
+        char* c = strToken;
+        
+        for (int i = 0; i < 5; i++)
+          print_scrolling_text(c, 30);
+        break;
+        
+      case '1': //Drawing //1,12,24 //1\012\024 while !null
+        strToken = strtok(request_id, ",");
+        int x = 0;
+        int y = 0;
+        
+        strToken = strtok(NULL, ",");
+        x = atoi(strToken);
+        strToken = strtok(NULL, ",");
+        y = atoi(strToken);
+        
+        led_module.writePixel(x,y,GRAPHICS_NORMAL,1);
+        
+        break;
+      default:
+        break;
+    }
   }
 }
--- osc/simple_server_osc.py
+++ osc/simple_server_osc.py
@@ -4,20 +4,41 @@ import serial
 
 ser = serial.Serial(port='/dev/ttyACM0', baudrate=9600, timeout=.1)
 
+def is_string(elem):
+    return type(elem) is str
+
 def print_handler(address, *args):
     for elem in args:
         if elem:
             print("->", elem, type(elem))
-            ser.write(elem.encode())
     print(f"{address}: {args}")
 
 
 def default_handler(address, *args):
     print(f"DEFAULT {address}: {args}")
 
+def scroll_handler(address, *args):
+    str2Send = "0,"
+    for elem in args:
+        if is_string(elem):
+            str2Send += elem
+        print("->", elem, type(elem))
+    print("Formatted string sent: " + str2Send)
+    ser.write(str2Send.encode())
+
+def draw_handler(address, *args):
+    str2Send = "1,"
+    for elem in args:
+        elem = int(elem)
+        str2Send += str(elem)
+        str2Send += ","
+        print("->", elem, type(elem))
+    print("Formatted string sent" + str2Send)
+    ser.write(str2Send.encode())
 
 dispatcher = Dispatcher()
-dispatcher.map("*", print_handler)
+dispatcher.map("/input/draw", draw_handler)
+dispatcher.map("/text/scroll", scroll_handler)
 dispatcher.set_default_handler(default_handler)
 
 ip = "127.0.0.1"

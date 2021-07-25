259d48ec477813367c4afe1d0e94fbe6f3f4d200
Lapin <serprev.z@gmail.com>
2021-07-25 15:42:32 +0200

    feat: adapt image bmp to internal image format
    transform bmp rgb 3*8bit of color resolution to internal format with 1
    color and 4 bit of resolution. Each byte contain 2 values of 4 bit. The
    first value is in the lowest weight bit, (the first bits).

--- P10_images/hex2matrix.py
+++ P10_images/hex2matrix.py
@@ -3,8 +3,8 @@ import numpy
 from PIL import Image
 
 if (len(sys.argv) != 2):
-    print("Usage:\npython3 img2matrix.py [FILE]");
-    sys.exit(2);
+    print("Usage:\npython3 img2matrix.py [FILE]")
+    sys.exit(2)
 
 image = Image.open(sys.argv[1])
 image.convert("RGB")
@@ -13,11 +13,22 @@ pixel_values = list(image.getdata())
 
 black_and_white = []
 
-for item in pixel_values:
-    for i in range (0, 3):
-        if (item[i] != 0):
-            black_and_white.append(0);
-        else:
-            black_and_white.append(1);
+#for item in pixel_values:
+#    for i in range (0, 3):
+#        if (item[0] != 0):
+#           black_and_white.append(item[0]);
+#        else:
+#            black_and_white.append(0);
+
+a = 0
+b = 0
+
+
+for i, item in enumerate(pixel_values):
+    if (i % 2) == 0:
+        a = (item[0] >> 4) & 0xF
+    else:
+        b = (item[0] >> 4) & 0xF
+        black_and_white.append((b << 4) | a )
 
 print(str(black_and_white).strip('[]'));
